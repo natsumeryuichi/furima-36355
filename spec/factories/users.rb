c85bc9e00d78a88a5a3a531a35651d76f42c6ef4
natsumeryuichi <a031133@gmail.com>
2021-12-12 13:06:16 +0900

    単体テストコードの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation {password}
+    first_name { Gimei.first.kanji }
+    last_name { Gimei.last.kanji }
+    first_name_kana { Gimei.first.katakana }
+    last_name_kana { Gimei.last.katakana }
+    birth_day { Faker::Date.backward }
+  end
+end
