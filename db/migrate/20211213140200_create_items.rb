3c558f427529b41fd2fcc7ac2b68c570f723cb25
natsumeryuichi <a031133@gmail.com>
2021-12-14 13:49:57 +0900

    カラムを編集

--- db/migrate/20211213140200_create_items.rb
+++ db/migrate/20211213140200_create_items.rb
@@ -3,15 +3,15 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
 
       t.timestamps
-      t.references :user,           mull: false, foreign_key: true
-      t.string :string,             null: false
-      t.text :explanation,          null: false
-      t.integer :category,          null: false
-      t.integer :condition,         null: false
-      t.integer :shipping_charge,   null: false
-      t.integer :prefecture,        null: false
-      t.integer :shipping_day,      null: false
-      t.integer :price,             null: false
+      t.references :user,              mull: false, foreign_key: true
+      t.string :string,                null: false
+      t.text :explanation,             null: false
+      t.integer :category_id,          null: false
+      t.integer :condition_id,         null: false
+      t.integer :shipping_charge_id,   null: false
+      t.integer :prefecture_id,        null: false
+      t.integer :shipping_day_id,      null: false
+      t.integer :price,                null: false
     end
   end
 end
