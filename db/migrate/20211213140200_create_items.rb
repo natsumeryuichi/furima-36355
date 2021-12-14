ae4d33c1b102ff8ceaafc55d5ec55cddc4deaf6e
natsumeryuichi <a031133@gmail.com>
2021-12-14 15:16:06 +0900

    ビューを編集

--- db/migrate/20211213140200_create_items.rb
+++ db/migrate/20211213140200_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
 
       t.timestamps
       t.references :user,              mull: false, foreign_key: true
-      t.string :string,                null: false
+      t.string :title,                null: false
       t.text :explanation,             null: false
       t.integer :category_id,          null: false
       t.integer :condition_id,         null: false
