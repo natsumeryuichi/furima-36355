d0a6c763d25645fa7285fafdb97128f74a84a95e
natsumeryuichi <a031133@gmail.com>
2021-12-14 13:45:17 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :price, presence: true
+
+  belongs_to :user
 end
