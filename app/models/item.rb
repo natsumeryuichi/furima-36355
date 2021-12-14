9769fafac519197aff603ccf778561d0aba1a036
natsumeryuichi <a031133@gmail.com>
2021-12-15 05:29:04 +0900

    商品出品機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,11 @@ class Item < ApplicationRecord
   validates :price, presence: true
 
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_day
 end
