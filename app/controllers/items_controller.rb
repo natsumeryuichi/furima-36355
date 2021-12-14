a22edbc2b13e07c0c2eaeac4d321b4ae7c04f237
natsumeryuichi <a031133@gmail.com>
2021-12-15 05:29:04 +0900

    商品出品機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,23 @@
 class ItemsController < ApplicationController
   def index
-    @item = Item.all
+    @items = Item.all
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
-
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+end
+
+private
+def items_params
+  params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
 end
