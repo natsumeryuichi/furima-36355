6f18ea8aee8abc41c99cf8dbe1b84be825c1a6a1
natsumeryuichi <a031133@gmail.com>
2021-12-15 05:40:16 +0900

    価格のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {greter_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 
   belongs_to :user
   has_one_attached :image
