2f3d88ee50abe842076f6959f880ccedc46a6ab5
natsumeryuichi <a031133@gmail.com>
2021-12-17 16:27:31 +0900

    テストコードを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
   validates :title, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
@@ -6,7 +7,8 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :price, presence: true, numericality: {greter_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999}, 
+                                    format: { with: /\A[0-9]+\z/, message: "は半角数字で入力してください"}
 
   belongs_to :user
   has_one_attached :image
--- spec/factories/prefectures.rb
+++ spec/factories/prefectures.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :prefecture do
-    
+    prefectures {Faker::Number.between(from: 1, to: 48)}
   end
 end
