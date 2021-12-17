65cddbeb53e6baa51100bac13f5d814775442d27
natsumeryuichi <a031133@gmail.com>
2021-12-17 16:27:31 +0900

    テストコードを実装

--- spec/factories/conditions.rb
+++ spec/factories/conditions.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :condition do
-    
+    conditions {Faker::Number.between(from: 1, to: 7)}
   end
 end
---
+++ spec/factories/item.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :item do
+    title {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
+    explanation {Faker::Lorem.characters(number: 100, min_alpha: 1, min_numeric: 1)}
+    category_id {Faker::Number.between(from: 2, to: 12)}
+    condition_id {Faker::Number.between(from: 2, to: 7)}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    shipping_charge_id {Faker::Number.between(from: 2, to: 3)}
+    shipping_day_id {Faker::Number.between(from: 2, to: 4)}
+    price {Faker::Number.between(from: 1, to: 10_000_000)}
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
