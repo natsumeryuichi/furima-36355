b384ccce4021c2812e357131e00c51a945df5628
natsumeryuichi <a031133@gmail.com>
2021-12-20 09:14:40 +0900

    他ユーザーが編集ページに遷移しようとするとトップページに遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :diffrent_user, only: [:edit, :update]
 
     def index
     @items = Item.all.order(created_at: :desc)
@@ -24,16 +25,17 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless @item.user_id == current_user.id
-      redirect_to root_path
-    else
-      render :edit
-    end
   end
 
   def update
     @item = Item.find(params[:id])
     @item.update(items_params)
+  end
+
+  def diffrent_user
+  item = @item = Item.find(params[:id])
+  unless item.user_id == current_user.id
+    redirect_to action: :index
   end
   
 end
@@ -42,4 +44,6 @@ end
 private
 def items_params
   params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+end
+
 end
