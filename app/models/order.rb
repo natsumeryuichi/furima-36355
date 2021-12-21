6b04f63b3e48fd165b27ec866cd207a04310524a
natsumeryuichi <a031133@gmail.com>
2021-12-21 21:23:31 +0900

    バリデーションを設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,12 @@
 class Order < ApplicationRecord
+  validates :postal_code, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :city, presence: true
+  validates :adress, presence: true
+  validates :bulding_name
+  validates :phone_number, presence: true
+  
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
