36e66a4c0b0b773ba6fd460c26a5ac6bd42dadff
natsumeryuichi <a031133@gmail.com>
2021-12-21 21:06:30 +0900

    カラムを作成

--- db/migrate/20211221110558_create_orders.rb
+++ db/migrate/20211221110558_create_orders.rb
@@ -2,6 +2,14 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
 
+      t.string     :postal_code,          null: false
+      t.integer    :prefecrure_id,        null: false
+      t.string     :city,                 null: false
+      t.string     :adress,               null: false
+      t.string     :building_name,        null: false
+      t.string     :phone_number,         null: false
+      t.references :purchase_management,  null: false,foreign_key: true
+
       t.timestamps
     end
   end
