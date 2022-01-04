04a94e18e1f6704317815c68829a65586499c4b4
natsumeryuichi <a031133@gmail.com>
2022-01-04 19:42:25 +0900

    マイグレーションファイルを編集

--- db/migrate/20211221110558_create_orders.rb
+++ db/migrate/20211221110558_create_orders.rb
@@ -6,7 +6,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.integer    :prefecture_id,        null: false
       t.string     :city,                 null: false
       t.string     :address,               null: false
-      t.string     :building_name,        null: false
+      t.string     :building_name
       t.string     :phone_number,         null: false
       t.references :purchase_management,  null: false,foreign_key: true
 
