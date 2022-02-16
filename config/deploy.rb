61af07be295a26960fe3bfc7f28dd810e5f0825e
natsumeryuichi <a031133@gmail.com>
2022-02-16 15:11:53 +0900

    deploy.rbを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.16.0"
 
 set :application, 'furima-36355'
-set :repo_url, 'git@example.com:natsumeryuichi/furima-36355.git'
+set :repo_url, 'git@github.com:natsumeryuichi/furima-36355.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
