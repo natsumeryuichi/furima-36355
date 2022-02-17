967507d6b0a14c1dcaef1db4759f86472a8ec891
natsumeryuichi <a031133@gmail.com>
2022-02-17 19:52:27 +0900

    コメントチャネルの不足分を追加

---
+++ app/channels/comment_channel.rb
@@ -0,0 +1,10 @@
+class CommentChannel < ApplicationCable::Channel
+  def subscribed
+    @item = Item.find(params[:item_id]) # 追記
+    stream_for @item    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
